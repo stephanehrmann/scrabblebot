01ad3ad301266e3b87d1ef54f0f33e3a71a58d22
Stephan <se@stephanehrmann.de>
2026-04-16 09:22:51 +0200

    Remove LETTER_VALUES constant
    Removed the LETTER_VALUES constant from App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import React, { useMemo, useRef, useState } from "react";
 
-const LETTER_VALUES = {
-  A:1, Ä:6, B:3, C:4, D:1, E:1, F:4, G:2, H:2,
-  I:1, J:6, K:4, L:2, M:3, N:1, O:2, Ö:8,
-  P:4, Q:10, R:1, S:1, T:1, U:1, Ü:6,
-  V:6, W:3, X:8, Y:10, Z:3
-};
 
 const BOARD_SIZE = 15;
 const CENTER = 7;
